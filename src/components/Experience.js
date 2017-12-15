40661ba485835f7613cd3e9ccf1f4d9d616b6a73
Stephen Woosley <stephenrwoosley@gmail.com>
2017-12-15 10:49:43 -0500

    updating scrolling experience and further componentizing

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,34 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ExperienceOne from './Experience/experience-one';
+import ExperienceTwo from './Experience/experience-two'
+import ExperienceThree from './Experience/experience-three'
 import 'bulma/css/bulma.css';
 
-const Experience = (props) => {
-  return(
-    <div>
-      <section className="hero is-medium hero-experience">
-        <div className="hero-body">
-          <div className="container">
-            <h3 className="title experience-content">
-              <div className="experience-one">
-                I’ve worked in the <span className="coral-span">education world</span> since 2007, in both a <span className="coral-span">wide variety of roles</span>—from <span className="coral-span">instruction</span> to accounting to course design to <span className="coral-span">web development</span>—and ranging a <span className="coral-span">wide variety of age groups</span>—from kindergarteners to <span className="coral-span">university students</span> to middle-aged adults.
+class Experience extends Component {
+  
+    state = {
+      oneClass: 'show',
+      twoClass: 'hidden',
+      threeClass: 'hidden'
+    }
+    componentDidMount() {
+      window.onscroll = () => this.handleScroll()
+    }
+
+    handleScroll() { 
+      if (document.documentElement.scrollTop > 430) {
+         this.setState({
+           twoClass: 'show', oneClass: 'hide'
+         })
+       }
+       else if (document.documentElement.scrollTop > 430) {
+         this.setState({
+           threeClass: 'show', twoClass: 'hide'
+         })
+       }
+     }
+
+    render() {
+      return(
+        <div>
+          <section className="hero is-medium hero-experience">
+            <div className="hero-body">
+              <div className="container">
+                <h3 className="title experience-content">
+                  <ExperienceOne oneClass={this.state.oneClass}/>
+                  <ExperienceTwo twoClass={this.state.twoClass}/>
+                  <ExperienceThree threeClass={this.state.threeClass}/>
+                </h3>
               </div>
-              <div className="scroll-btn">
-                <a href="#experience-two" className="scroll-button">More...</a>
-              </div>    
-              <div className="experience-two" id="experience-two">
-                In 2014 I began <span className="coral-span">moonlighting</span> as a developer-in-training, and began working for a string of <span className="coral-span">small startups</span>. I even <span className="coral-span">co-founded a coding bootcamp</span> in 2016.
-              </div>
-              <div className="scroll-btn">
-                <a href="#experience-three" className="scroll-button">More...</a>
-              </div>
-              <div className="experience-three" id="experience-three">
-                These days I am the <span className="coral-span">lead teaching assistant</span> for the <span className="coral-span">Coding Bootcamp</span> at UNC Chapel Hill, and do <span className="coral-span">instructional design technology</span> for UNC’s <span className="coral-span">Kenan-Flagler Business School</span>. I can't wait to see what's next.
-              </div>
-            </h3>
-          </div>
+            </div>
+          </section>
         </div>
-      </section>
-    </div>
-  );
+      );
+    }
 }
 
 export default Experience;
---
+++ src/components/Experience/experience-three.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bulma/css/bulma.css';
+
+const ExperienceThree = (props) => {
+  return(
+    <div>
+      <div className="experience-three" id="experience-three">
+        These days I am the <span className="coral-span">lead teaching assistant</span> for the <span className="coral-span">Coding Bootcamp</span> at UNC Chapel Hill, and do <span className="coral-span">instructional design technology</span> for UNC’s <span className="coral-span">Kenan-Flagler Business School</span>. I can't wait to see what's next.
+      </div>
+    </div>
+  );
+}
+
+export default ExperienceThree;
